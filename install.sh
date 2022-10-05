20c752f2904305e9b15602313eea8df2d6ee4d14
Silei Xu <ray.slxu@gmail.com>
2022-10-05 16:26:26 -0700

    Fix permission

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -x
 set -o pipefail
 
 if ! test -d qald ; then
+	chmod +x $HOME $PWD
+	chown -R genie-toolkit:genie-toolkit .
+
 	git clone https://github.com/rayslxu/qald
 	chown genie-toolkit:genie-toolkit qald
 

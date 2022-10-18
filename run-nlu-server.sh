3d204c81861c73015a64c89d3b53d13b4994d6b7
Silei Xu <ray.slxu@gmail.com>
2022-10-18 00:05:31 -0700

    Add environment variable for bootleg

--- run-nlu-server.sh
+++ run-nlu-server.sh
@@ -4,6 +4,8 @@ set -e
 set -o pipefail
 set -x
 
-exec node --experimental_worker ../genie-toolkit/dist/tool/genie.js server \
+export GENIENLP_DATABASE_DIR=./database/
+
+exe node --experimental_worker ../genie-toolkit/dist/tool/genie.js server \
   --nlu-model "file://models/$1/" \
   --thingpedia "manifest.tt" 

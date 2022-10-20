db1743d0b29fe4d8f2f201cc0bd4536b482aca3e
Silei Xu <ray.slxu@gmail.com>
2022-10-20 15:40:56 -0700

    Change genie-server version in setup script

--- demo/setup.sh
+++ demo/setup.sh
@@ -7,6 +7,7 @@ set -o pipefail
 if ! test -d genie-server ; then
 	git clone https://github.com/stanford-oval/genie-server.git
 	pushd genie-server > /dev/null
+	git checkout wip/qald
 	npm ci
 	popd
 fi

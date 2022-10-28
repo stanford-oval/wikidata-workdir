6af12a06db848b14ca30331af2d4b3cf293879c8
Silei Xu <ray.slxu@gmail.com>
2022-10-28 14:45:17 -0700

    Add caching for gpt3 evaluation

--- openai/evaluate.py
+++ openai/evaluate.py
@@ -12,28 +12,43 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', type=str, required=True, help='File to evaluate')
     parser.add_argument('-o', '--output', type=str, required=False, help='File to write the result, default to the same name as the input file')
     parser.add_argument('--model', type=str, required=True, help='ID of the model')
+    parser.add_argument('--cache', type=str, required=False, default='cache.json', help='Path to the cache file')
     args = parser.parse_args()
-    
+
+    if os.path.exists(args.cache):
+        cache = json.load(open(args.cache))
+    else:
+        cache = dict()
+
     correct = 0
     total = 0
     with open(args.input, 'r') as fin, open(args.output, 'w') as fout:
         for line in fin:
             example = json.loads(line)
-            response = openai.Completion.create(
-                model = args.model,
-                prompt = example['prompt'],
-                stop = RESPONSE_END_TOKEN,
-                max_tokens = 100
-            )
-            print(response)
-            example['gold'] = example['completion']
-            example['completion'] = response['choices'][0]['text']
-            if example['completion'] == example['gold']:
+            
+            # check cache, if not available, query openai
+            if example['prompt'] in cache:
+                prediction = cache[example['prompt']]
+            else:
+                response = openai.Completion.create(
+                    model = args.model,
+                    prompt = example['prompt'],
+                    stop = RESPONSE_END_TOKEN,
+                    max_tokens = 100
+                )
+                prediction = response['choices'][0]['text']
+                cache[example['prompt']] = prediction
+
+            # check exact match
+            if example['completion'] == prediction:
                 correct += 1
                 break
 
             total += 1
-            fout.write(json.dumps(example) + '\n')
+            fout.write('{}\t{}\t{}\t{}\n'.format(example['id'], example['utterance'], example['completion'].strip(), prediction.strip()))
     
+    with open(args.cache, 'w') as f:
+        json.dump(cache, f)
     print(correct, total)
+
 
